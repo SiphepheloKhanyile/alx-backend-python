c0d60c6b43d3b2f03b25d2dd02654eb72d85ba6f
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-06-10 02:11:39 +0200

    add: Task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+type-annotated function `to_str` that takes a float `n` as argument
+and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Args:
+        n (float): float
+    Returns:
+        str: string representation of `n`
+    """
+    return str(n)
