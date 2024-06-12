7fc7eb4995687e3b4edd5437936eb7be4005515b
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-06-12 18:27:54 +0200

    add: Task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+using `wait_random`
+"""
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int):
+    """
+    Purpose: one
+    """
+    delays_list = []
+    i = 0
+    while i < n:
+        task = await wait_random(max_delay)
+        delays_list.append(task)
+        i += 1
+    # end while
+    return delays_list
+# end def
