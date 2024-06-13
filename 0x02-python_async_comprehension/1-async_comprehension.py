53d968b049d3de9ad2588678a1561ab696700a50
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-06-13 20:50:12 +0200

    add: Task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Async Comprehensions
+"""
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Returns:
+        List[float]: List of 10 random numbers.
+    """
+    return [i async for i in async_generator()]
