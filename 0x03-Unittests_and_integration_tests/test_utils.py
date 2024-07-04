ff71dd4264eca111053ac08d37121a4348498c68
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-07-04 02:33:33 +0200

    add: Add Task 0

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""
+Test `utils.py`
+"""
+import unittest
+from unittest.mock import patch, Mock
+from parameterized import parameterized  # pylint: disable=E0401
+from utils import access_nested_map, get_json, memoize
+from client import GithubOrgClient  # pylint: disable=W0611
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """first unit test for utils.access_nested_map
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test accessing a value in a nested map with a key path.
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test accessing a value in a nested map with a key path.
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """second unit test for utils.get_json
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://example.com/1", {"payload": False}),
+    ])
+    def test_get_json(self, url, expected):
+        """Test get_json
+        """
+        with patch('utils.requests.get') as mock_get:
+            mock_get.return_value = Mock()
+            mock_get.return_value.json.return_value = expected
+            self.assertEqual(get_json(url), expected)
+
+
+class TestMemoize(unittest.TestCase):
+    """third unit test for utils.memoize
+    """
+    def test_memoize(self):
+        """Test memoize
+        """
+        class TestClass:
+            """test class"""
+            def a_method(self):
+                """test method"""
+                return 42
+
+            @memoize
+            def a_property(self):
+                """test method"""
+                return self.a_method()
+        with patch.object(TestClass, 'a_method') as mock:
+            test = TestClass()
+            test.a_property  # pylint: disable=W0104
+            test.a_property  # pylint: disable=W0104
+            mock.assert_called_once()
